1b0f0c613be4354ea93c866f1506c2602df1bff8
Alex Rodrigo <arodrigozaragoza@gmail.com>
2024-12-13 19:35:29 +0100

    Minijuego Coche Parte 3

---
+++ Motores_unity_6/Assets/3.Scripts/Minigames/Car/CarMinigameController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class CarMinigameController : MonoBehaviour
+{
+    private int neededWheels = 4;
+
+    private GameObject carSide1;
+    private GameObject carSide2;
+
+    private void FlipCar()
+    {
+
+    }
+}
