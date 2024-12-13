1bdb8ffd859c6a1593f08f9611a5ae9089f5ab96
salong01 <salong01@estudiantes.unileon.es>
2024-12-13 15:03:31 +0100

    Game manager con control de acciones y pausa de juego

--- Motores_unity_6/Assets/3.Scripts/GameManager.cs
+++ Motores_unity_6/Assets/3.Scripts/GameManager.cs
@@ -1,16 +1,62 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+public class GameManager : MonoBehaviour {
+
+    public static GameManager Instance { get; private set; }
+    public enum PlayerAction { Idle, Walking, Running, Action, Bend}
+    public AudioSource backgroundMusic;
+    public AudioSource nightSounds;
+    public AudioSource monsterSound;
+    public float pausedVolume = 0.2f;
+    private float originalVolume;
+
+    private bool isPaused = false;
+    public PlayerAction currentAction = PlayerAction.Idle;
+    public bool alive = true;
+
+    void Awake(){
+        if (Instance != null && Instance != this)
+            Destroy(gameObject);
+        else{
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            PauseGame();
+    }
+    
+    public void PauseGame() {
+
+        isPaused = !isPaused;
+        if (isPaused) {
+            originalVolume = monsterSound.volume;
+            Time.timeScale = 0f;
+            backgroundMusic.Pause();
+            nightSounds.Pause();
+            monsterSound.volume = pausedVolume;
+            //SceneManager.LoadScene("EscenaMinijuegoCables");
+            Debug.Log("Juego en pausa");
+        }
+        else {
+            Time.timeScale = 1f;
+            //SceneManager.LoadScene("EscenaGasolineraConIAGameManager");
+            backgroundMusic.UnPause();
+            nightSounds.UnPause();
+            monsterSound.volume = originalVolume;
+            Debug.Log("Juego reanudado");
+        }
+    }
+
+    public void SetPlayerAction(PlayerAction action) {
+        currentAction = action;
+    }
+
+    public void dead() {
+        alive = false;
+
     }
 }
