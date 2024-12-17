a50ed16c2a37e0473b025e2311dc63dfe6f980a4
Rymol <101354777+Rymol@users.noreply.github.com>
2024-12-17 15:40:14 +0100

    CC cambia entre animaciones
    Idle, Walk and Run

--- Motores_unity_6/Assets/3.Scripts/Player/Scripts/PlayerAnimation.cs
+++ Motores_unity_6/Assets/3.Scripts/Player/Scripts/PlayerAnimation.cs
@@ -10,15 +10,18 @@ namespace FinalCharacterController
         [SerializeField] private float LocomotionBlendSpeed = 0.02f;
 
         private PlayerLocomotionInput _playerLocomotionInput;
+        private PlayerState _playerState;
 
         private static int inputXHash = Animator.StringToHash("inputX");
         private static int inputYHash = Animator.StringToHash("inputY");
+        private static int inputMagnitudeHash = Animator.StringToHash("inputMagnitude");
 
         private Vector3 _currentBlendInput = Vector3.zero;
 
         private void Awake()
         {
             _playerLocomotionInput = GetComponent<PlayerLocomotionInput>();
+            _playerState = GetComponent<PlayerState>();
         }
 
         private void Update()
@@ -28,11 +31,15 @@ namespace FinalCharacterController
 
         void UpdateAnimationState()
         {
-            Vector2 inputTarget = _playerLocomotionInput.MovementInput;
+            bool isSprinting = _playerState.CurrentPlayerMovementState == PlayerMovementState.Running;
+
+            Vector2 inputTarget = isSprinting ? _playerLocomotionInput.MovementInput * 1.5f : _playerLocomotionInput.MovementInput;
             _currentBlendInput = Vector3.Lerp(_currentBlendInput, inputTarget, LocomotionBlendSpeed * Time.deltaTime);
+
 
             _animator.SetFloat(inputXHash, _currentBlendInput.x);
             _animator.SetFloat(inputYHash, _currentBlendInput.y);
+            _animator.SetFloat(inputMagnitudeHash, _currentBlendInput.magnitude);
         }
 
     }
